1c2f6aab2f9062c8b42945d604869a5f347c5be0
Nahuel Benitez <jorgenahuelbenitez07@gmail.com>
2024-06-03 10:38:08 -0300

    move repl to src, and correct imports

--- src/repl.ts
+++ src/repl.ts
@@ -1,5 +1,5 @@
 import { repl } from '@nestjs/core';
-import { AppModule } from '../modules/app/app.module'
+import { AppModule } from './modules/app/app.module'
 
 async function bootstrap() {
   await repl(AppModule);
